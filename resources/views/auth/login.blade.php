2ed5706be4c1ab8113892c2c4b4c180a889ffebc
Allhassane Coulibaly <allhassane.coulibaly@sgpme.ci>
2026-02-28 10:24:24 +0000

    maj

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,7 +21,7 @@
         </div>
     @endif
 
-    <form method="POST" action="{{ route('login') }}">
+    <form method="POST" action="{{ route('login') }}" class="loginForm">
         @csrf
 
         <!-- Email -->
